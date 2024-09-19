e83b32ae6bf428c36bec7543455105085a93869c
Rayad Kubaisi <rayad.kubaisi@gmail.com>
2024-09-19 13:51:43 +0000

    Update settings.py

--- teachermarks/settings.py
+++ teachermarks/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'teachermarks.wsgi.application'
 ###################
 DATABASES = {
      "default": {
-         "ENGINE": 'django.db.backends.mysql',
+         "ENGINE": 'mysql.connector.django',
 #     "ENGINE": os.environ.get("SQL_ENGINE"),
          "NAME": os.environ.get("SQL_DATABASE"),
          "USER": os.environ.get("SQL_USER"),
